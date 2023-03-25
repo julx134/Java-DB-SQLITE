8e4e251b617ac7fb4b25dade704d4f0fa16969aa
julx134 <julesgammad56@gmail.com>
2023-03-25 15:40:58 -0400

    added connection method to return jdbc connectio object

--- src/main/java/TMURecApp.java
+++ src/main/java/TMURecApp.java
@@ -1,5 +1,29 @@
 package main.java;
 
-public class TMURecApp {
+import java.sql.*;
 
+public class TMURecApp {
+	
+	
+	private Connection openConnection() {
+		Connection c = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:resource/test.db");
+		} catch (Exception e) {
+			System.err.println("Problem Encountered");
+		}
+		System.out.println("Opened database successfully");
+		return c;
+	}
+
+	public static void main(String args[]) {
+		//intialize class object
+		TMURecApp app = new TMURecApp();
+		
+		//get connection
+		Connection c = app.openConnection();
+		
+		
+	}
 }
